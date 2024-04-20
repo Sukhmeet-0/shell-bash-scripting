a3c6e92c21a4611b0df9d78f16e633cf8bccecd7
Sukhmeet-0 <xsukhmeet@gmail.com>
2024-04-20 11:09:29 +0530

    adding arrays variables print and input in bash scripting

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Bash/ shell scripting
+
+echo "hello world"
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+name=Mark
+val=10
+echo The name is $name
+echo Value is $val
+
+echo "Enter your name: "
+read naam
+echo "Entered name is: $naam"
---
+++ tut2.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# echo "enter names:"
+# read name1 name2 name3
+# echo "names: $name1, $name2, $name3"
+
+# read -p'username: ' user_var
+# /=read -p 'username: ' user_var
+# read -sp'user password: ' user_pass
+
+# echo $user_var
+# echo $user_pass
+
+# echo "enter names: "
+# read -a names
+
+# echo ${names[0]}, ${names[1]}
+
+echo "enter name: "
+read 
+echo "Name: " $REPLY
